9fb8a24843464297982d7ce96c21441f208f295e
eL Setiyawan <el.setiyawan@gmail.com>
2021-04-27 16:15:17 +0700

    service init

--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -1,35 +1,45 @@
 import { Request } from "express";
+import APIError from "helpers/APIError";
 import { v4 } from "uuid";
 
+interface iTodo {
+  id: string;
+  desciption: string;
+  deadline: Date;
+  done: boolean;
+}
+
+interface iRequestData {
+  desciption: string;
+  deadline: Date | string;
+  done: boolean;
+}
+
+let todos: iTodo[] = [];
+
 class TodoService {
-  body: Request["body"];
-  params: Request["params"];
-  todos: Array<{
-    id: string;
-    description: string;
-    deadline: Date;
-    done: boolean;
-  }>;
-
-  constructor(req: Request) {
-    this.body = req.body;
-    this.params = req.params;
-    this.todos = [];
-  }
-
-  getAllTodo = () => {
-    return this.todos;
+  getAllTodo = (): iTodo[] => {
+    return todos;
   };
 
-  create = () => {
+  create = (reqData: iRequestData): iTodo => {
     const data = {
+      ...reqData,
       id: v4(),
-      description: this.body.description,
-      deadline: new Date(this.body.deadline),
-      done: this.body.done,
+      deadline: new Date(reqData.deadline),
     };
-    this.todos.push(data);
+
+    todos.push(data);
     return data;
+  };
+
+  getSingleTodoById = (id: string): void | iTodo => {
+    const singleTodo = todos.find((todo) => todo.id === id);
+    if (!singleTodo) {
+      throw new APIError({ message: "todo not found" });
+    } else {
+      return singleTodo;
+    }
   };
 }
 
